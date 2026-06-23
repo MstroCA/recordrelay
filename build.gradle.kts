306314f6a1d0de4a844f822bc1697fb13b34f493
MstroCA <cemakar18@gmail.com>
2026-06-23 15:10:57 +0300

    fix: use releaseVersion property to correctly override version at tag time

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     group = "io.recordrelay"
-    version = "0.1.0-SNAPSHOT"
+    version = findProperty("releaseVersion")?.toString() ?: "0.1.0-SNAPSHOT"
     repositories {
         mavenCentral()
     }
